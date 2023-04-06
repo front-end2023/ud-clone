ab1d27424256cb90befec37b23735fda92f2ee51
User <keyloogerpy2022@gmail.com>
2023-04-06 13:56:12 +0300

    drop menu

---
+++ js/main.js
@@ -0,0 +1,6 @@
+function openLang(){
+    document.getElementById("lang-window").style.display = "block";
+}
+function closeLang(){
+    document.getElementById("lang-window").style.display = "none";
+}
