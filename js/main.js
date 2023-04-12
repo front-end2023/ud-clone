7899a1ee2844138e8dc25c757746b9c86a61af77
ahmed anis <keyloggerpy2022@gmail.com>
2023-04-12 21:48:29 +0300

    list done

--- js/main.js
+++ js/main.js
@@ -4,3 +4,113 @@ function openLang(){
 function closeLang(){
     document.getElementById("lang-window").style.display = "none";
 }
+
+// show List two-1
+let subMenu_1 = document.getElementById("3_1");
+
+function hidde_1(){
+    subMenu_1.style.display="none";
+    document.getElementById("list3").style.display="none";
+}
+function show_1(){
+    subMenu_1.style.display="block";
+    document.getElementById("list3").style.display="block";
+}
+document.getElementById("1").addEventListener("mouseover",show_1);
+document.getElementById("1").addEventListener("mouseout",hidde_1);
+document.getElementById("4").addEventListener("mouseover",show_1);
+document.getElementById("4").addEventListener("mouseout",hidde_1);
+document.getElementById("7").addEventListener("mouseover",show_1);
+document.getElementById("7").addEventListener("mouseout",hidde_1);
+document.getElementById("10").addEventListener("mouseover",show_1);
+document.getElementById("10").addEventListener("mouseout",hidde_1);
+
+let subMenu_2 = document.getElementById("3_2");
+
+function hidde_2(){
+    subMenu_2.style.display="none";
+    document.getElementById("list3").style.display="none";
+}
+function show_2(){
+    subMenu_2.style.display="block";
+    document.getElementById("list3").style.display="block";
+}
+document.getElementById("2").addEventListener("mouseover",show_2);
+document.getElementById("2").addEventListener("mouseout",hidde_2);
+document.getElementById("5").addEventListener("mouseover",show_2);
+document.getElementById("5").addEventListener("mouseout",hidde_2);
+document.getElementById("8").addEventListener("mouseover",show_2);
+document.getElementById("8").addEventListener("mouseout",hidde_2);
+
+let subMenu_3 = document.getElementById("3_3");
+
+function hidde_3(){
+    subMenu_3.style.display="none";
+    document.getElementById("list3").style.display="none";
+}
+function show_3(){
+    subMenu_3.style.display="block";
+    document.getElementById("list3").style.display="block";
+}
+document.getElementById("3").addEventListener("mouseover",show_3);
+document.getElementById("3").addEventListener("mouseout",hidde_3);
+document.getElementById("6").addEventListener("mouseover",show_3);
+document.getElementById("6").addEventListener("mouseout",hidde_3);
+document.getElementById("9").addEventListener("mouseover",show_3);
+document.getElementById("9").addEventListener("mouseout",hidde_3);
+
+
+let subMenu_4 = document.getElementById("3_4");
+
+function hidde_4(){
+    subMenu_4.style.display="none";
+    document.getElementById("list3").style.display="none";
+}
+function show_4(){
+    subMenu_4.style.display="block";
+    document.getElementById("list3").style.display="block";
+}
+document.getElementById("11").addEventListener("mouseover",show_4);
+document.getElementById("11").addEventListener("mouseout",hidde_4);
+document.getElementById("14").addEventListener("mouseover",show_4);
+document.getElementById("14").addEventListener("mouseout",hidde_4);
+document.getElementById("17").addEventListener("mouseover",show_4);
+document.getElementById("17").addEventListener("mouseout",hidde_4);
+document.getElementById("20").addEventListener("mouseover",show_4);
+document.getElementById("20").addEventListener("mouseout",hidde_4);
+
+let subMenu_5 = document.getElementById("3_5");
+
+function hidde_5(){
+    subMenu_5.style.display="none";
+    document.getElementById("list3").style.display="none";
+}
+function show_5(){
+    subMenu_5.style.display="block";
+    document.getElementById("list3").style.display="block";
+}
+document.getElementById("12").addEventListener("mouseover",show_5);
+document.getElementById("12").addEventListener("mouseout",hidde_5);
+document.getElementById("15").addEventListener("mouseover",show_5);
+document.getElementById("15").addEventListener("mouseout",hidde_5);
+document.getElementById("18").addEventListener("mouseover",show_5);
+document.getElementById("18").addEventListener("mouseout",hidde_5);
+
+let subMenu_6 = document.getElementById("3_6");
+
+function hidde_6(){
+    subMenu_6.style.display="none";
+    document.getElementById("list3").style.display="none";
+}
+function show_6(){
+    subMenu_6.style.display="block";
+    document.getElementById("list3").style.display="block";
+}
+document.getElementById("13").addEventListener("mouseover",show_6);
+document.getElementById("13").addEventListener("mouseout",hidde_6);
+document.getElementById("16").addEventListener("mouseover",show_6);
+document.getElementById("16").addEventListener("mouseout",hidde_6);
+document.getElementById("19").addEventListener("mouseover",show_6);
+document.getElementById("19").addEventListener("mouseout",hidde_6);
+
+
